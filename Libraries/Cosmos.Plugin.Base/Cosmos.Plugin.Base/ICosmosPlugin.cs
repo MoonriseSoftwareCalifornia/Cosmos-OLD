d8929a9d1a72d7f7df6efabb30d6537a68c324f9
Eric Dean Kauffman <eric@moonrise.net>
2023-03-04 13:50:15 -0800

    Initial commit of plugin base

---
+++ Libraries/Cosmos.Plugin.Base/Cosmos.Plugin.Base/ICosmosPlugin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Cms.PluginBase
+{
+    /// <summary>
+    /// Interface for Cosmos Plugins
+    /// </summary>
+    public interface ICosmosPlugin
+    {
+        #region PROPERTIES
+        /// <summary>
+        /// Author of plugin
+        /// </summary>
+        string Author { get; }
+        /// <summary>
+        /// Author website
+        /// </summary>
+        Uri AuthorUrl { get; }
+        /// <summary>
+        /// Description of what the plugin does
+        /// </summary>
+        string Description { get; }
+        /// <summary>
+        /// Name of the plugin
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// Version of plugin
+        /// </summary>
+        string Version { get; }
+
+        /// <summary>
+        /// Paths to templates
+        /// </summary>
+        IEnumerable<string> TemplatePaths { get; }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// Configuration that gets loaded from settings
+        /// </summary>
+        /// <param name="config"></param>
+        void Config(string config);
+
+        /// <summary>
+        /// Execution method
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        string Execute(string[] args);
+
+        #endregion
+    }
+}
